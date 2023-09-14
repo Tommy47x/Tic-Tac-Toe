89c9b1872e6d677dad239cd81d025cef90a2798c
Tommy47x <110035943+Tommy47x@users.noreply.github.com>
2023-09-14 11:02:23 +0300

    Update script.js
    -modified the checkWinner function
    -created a function 'createButton' for the code that was 'free'.
    -also we call the 'createButton' function with window.onLoad so the first clicks will work

--- script.js
+++ script.js
@@ -4,45 +4,51 @@ const buttons = document.querySelectorAll('.btn-outline-primary'); // Constant f
 const message = document.getElementById('message'); // Constant for in-page message (Last-Move + Winner)
 let gameEnded = false; // Var for in-game progress (When = True game stops)
 
-for (let i = 0; i < buttons.length; i++) { // We activate the onClick function inside JS with this for
-    buttons[i].addEventListener('click', function () { // The onClick function
-        if (!gameEnded && this.textContent !== "X" && this.textContent !== "O") {
-            if (clickCounter % 2 === 0) { // Algorithm for choosing the player (1 round, each)
-                currentPlayer = "X" // First we start with X (First move)
-            } else {
-                currentPlayer = "O"; // Second move (O)
-            }
-            this.textContent = currentPlayer; // For Last-Move in: message
-            clickCounter++; // For stopping the game if it's draw (9 moves in total)
-            message.textContent = "Last: " + currentPlayer; // Last-Move
-            const winner = checkWinner(); // For the function declared down
-            if (winner) {
-                message.textContent = winner;
-                gameEnded = true;
+window.onload = createButton(); // Otherwise first clicks don't work
 
+function createButton() {
+    for (let i = 0; i < buttons.length; ++i) { // We activate the onClick function inside JS with this for
+        buttons[i].addEventListener('click', function () { // The onClick function
+            if (!gameEnded && this.textContent !== "X"
+                && this.textContent !== "O") {
+                if (clickCounter % 2 === 0) { // Algorithm for choosing the player (1 round, each)
+                    currentPlayer = "X" // First we start with X (First move)
+                } else {
+                    currentPlayer = "O"; // Second move (O)
+                }
+                this.textContent = currentPlayer; // For Last-Move in: message
+                message.textContent = "Last Move: " + currentPlayer; // Last-Move
+                const winner = checkWinner(); // For the function declared down
+                ++clickCounter; // For stopping the game if it's draw (9 moves in total)
+                if (clickCounter === 9) { // Draw Algorithm
+                    message.textContent = "It's a draw!"
+                }
+                if (winner) {
+                    message.textContent = winner;
+                    gameEnded = true;
+                }
             }
-        }
-    });
+        });
+    }
 }
 
-function checkWinner() { // Function that checks all the posibilities of winning
+function checkWinner() {
     const winningCombos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Lines
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
         [0, 4, 8], [2, 4, 6] // Diagonals
     ];
 
-    for (const combo of winningCombos) { // Algorithm for choosing the winner
-        const [a, b, c] = combo;
-        if (buttons[a].textContent === "X" && buttons[b].textContent === "X" && buttons[c].textContent === "X") {
-            return "X wins!"; // 3 moves correctly = Win for X
-        } else if (buttons[a].textContent === "O" && buttons[b].textContent === "O" && buttons[c].textContent === "O") {
-            return "O wins!";// 3 moves correctly = Win for O
+    const winConditions = { // Constant for win / loss condition
+        "XXX": "X wins!",
+        "OOO": "O wins!"
+    };
+
+    for (const combo of winningCombos) { // for every winning possibility
+        const comboText = combo.map(index => buttons[index].textContent).join(''); // map for index of button
+        if (winConditions[comboText]) { // 'join' up is for adding every letter to fit 'winConditions'.
+            return winConditions[comboText]; // return result
         }
     }
-    if (clickCounter === 9) { // Draw algorithm based on the number of total moves
-        return "It's a draw!";
-    }
 
-    return null;
 }
